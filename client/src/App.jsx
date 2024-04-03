f34efa5d412e8891b6d0d345cd21f7e06b62eb18
umesh2407 <choudharyumesh986@gmail.com>
2024-04-03 18:42:17 +0530

    add Create Post

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter,Link, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
 import{logo} from './assets';
 import { Home, CreatePost } from './pages';
 
--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import React, {useState} from 'react';
+import {useNavigate} from 'react-router-dom';
+import {preview} from '../assets';
+import {getRandomPrompt} from '../utils';
+import {FormField, Loader} from '../components';
 
 const CreatePost = () => {
+  const navigate = useNavigate();
+  const [form, setForm] = useState({
+    name:'',
+    prompt:'',
+    photo:'',
+  });
+  const [generatingImg, setGeneratingImg] = useState(false);
+  const [loading,setLoading]=useState(false);
+
+  const handleSubmit = ()=>{
+
+  }
+
+  const handleChange = (e)=>{
+
+  }
+
   return (
-    <div>CreatePost</div>
+    <section className='max-w-7xl mx-auto'>
+    <div className='font-extrabold text-[#222328] text-[32px]'>
+    <h1>Create</h1>
+    <p className="mt-2 text-[#666e75] text-[14px] max-w-[700px]">
+Create imaginative and visually stunning images through DALL-E AI and share with the community
+    </p>
+    </div>
+
+
+    </section>
   )
 }
 
