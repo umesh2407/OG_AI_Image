775afe994642f7ed2dd8c981e7ad78084ebdbd7b
umesh2407 <choudharyumesh986@gmail.com>
2024-04-07 18:36:34 +0530

    added routes

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -22,9 +22,9 @@ router.route('/').post(async (req, res) => {
 
     const aiResponse = await openai.createImage({
       prompt,
-      n: 1,
-      size: '1024x1024',
-      response_format: 'b64_json',
+      n: 1, //one image
+      size: '1024x1024', //image size
+      response_format: 'b64_json', 
     });
 
     const image = aiResponse.data.data[0].b64_json;
